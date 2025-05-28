270e12e75084564c18ccba9b40b41b354d61e593
晉安 劉 <n88068@hotmail.com>
2025-05-28 13:56:28 +0800

    增加JS

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using YumBlazor.Components;
 using YumBlazor.Components.Account;
 using YumBlazor.Data;
+using YumBlazor.Repository.IRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
---
+++ Services/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+using Mono.TextTemplating;
+
+namespace YumBlazor.Services.Extensions
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr","success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+
+    }
+}
