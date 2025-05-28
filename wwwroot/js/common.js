99af314c02e2779eed9f9a5595e8e8cb7d30f1a3
晉安 劉 <n88068@hotmail.com>
2025-05-28 21:13:11 +0800

    暫時上傳

---
+++ wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿function ShowConfirmationModal() {
+    /*const myModal = new bootstrap.Modal(document.getElementById('myModal'), options)*/
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal')).show();
+}
+
+function HideConfirmationModal() {    
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal')).hide();
+}
